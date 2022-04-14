38c74af89e8878081d704769db652af1c0b45fe3
Alex Ershov <AlexWE36@mail.ru>
2022-04-14 13:32:10 +0300

    extra tests

--- modules/deicstra_algorithm/test/test_ershov_alexey_deicstra_algorithm.cpp
+++ modules/deicstra_algorithm/test/test_ershov_alexey_deicstra_algorithm.cpp
@@ -6,8 +6,11 @@
 
 #include "./include/ershov_alexey_deicstra_algorithm.h"
 
-TEST(Algorithm, Test_Default_No_Throw) {
-    ASSERT_NO_THROW(Algorithm deicstra);
+TEST(Algorithm, Test_Default_No_Throw) { ASSERT_NO_THROW(Algorithm deicstra); }
+
+TEST(Algorithm, Test_Default_Copy_No_Throw) {
+  Algorithm deicstra;
+  ASSERT_NO_THROW(Algorithm simpleAlgorithm(deicstra));
 }
 
 TEST(Random_Vector, Test_Default_No_Throw) {
@@ -91,6 +94,41 @@ TEST(Deicstra_Algorithm, Test_First_Top_With_Static_Data) {
   Algorithm deicstra;
 
   std::vector<size_t> algorithmResult = deicstra.getDeicstra(graf, 0);
+  for (size_t i = 0; i < count; ++i) {
+    if (trueResult[i] != algorithmResult[i]) {
+      check = false;
+    }
+  }
+
+  ASSERT_EQ(check, true);
+}
+
+TEST(Deicstra_Algorithm, Test_No_Top_With_Static_Data) {
+  const std::vector<std::vector<size_t>> graf = {
+      {0, 7, 9, 0, 0, 14},  {7, 0, 10, 15, 0, 0}, {9, 10, 0, 11, 0, 2},
+      {0, 15, 11, 0, 6, 0}, {0, 0, 0, 6, 0, 9},   {14, 0, 2, 0, 9, 0}};
+  const std::vector<size_t> trueResult = {0, 7, 9, 20, 20, 11};
+  bool check = true;
+  const size_t count = graf.size();
+  Algorithm deicstra;
+
+  std::vector<size_t> algorithmResult = deicstra.getDeicstra(graf);
+  for (size_t i = 0; i < count; ++i) {
+    if (trueResult[i] != algorithmResult[i]) {
+      check = false;
+    }
+  }
+
+  ASSERT_EQ(check, true);
+}
+
+TEST(Deicstra_Algorithm, Test_Default_Top_With_Default_Data) {
+  const std::vector<size_t> trueResult = {0, 7, 9, 20, 20, 11};
+  bool check = true;
+  const size_t count = 6;
+  Algorithm deicstra;
+
+  std::vector<size_t> algorithmResult = deicstra.getDeicstra();
   for (size_t i = 0; i < count; ++i) {
     if (trueResult[i] != algorithmResult[i]) {
       check = false;
